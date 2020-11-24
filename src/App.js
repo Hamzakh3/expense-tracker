b915ce4bd7558c4484b1c099986877afd36376a4
Hamzakh3 <hamkh3@gmail.com>
2020-11-25 01:52:29 +0500

    remove unused stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import { useContext } from 'react';
 import './App.css';
 import AddTransaction from './components/addTransaction';
 import Balance from './components/balance';
 import Header from './components/header'
 import Total from './components/total'
 import Transactions from './components/transactions';
-import { Provider, RootContext } from './context/GlobalState';
+import { Provider } from './context/GlobalState';
 
 
 
